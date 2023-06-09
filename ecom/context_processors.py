ebfe4a33a3297cb3a3fba8dfc8ca6ef2b997459f
Yash <yschauhan098@gmail.com>
2023-06-09 23:13:35 +0530

    authentication

--- ecom/context_processors.py
+++ ecom/context_processors.py
@@ -1,7 +1,11 @@
 from .models import Cart
+from django.contrib.auth.models import User
 
 def cart_count(request):
-    accountuser = request.user
-    products = Cart.get_all_by_user(accountuser)
-    count = products.count()
-    return {'cart_count': count}
+    if request.user.isauthencated:
+        accountuser = request.user
+        products = Cart.get_all_by_user(accountuser)
+        count = products.count()
+        return {'cart_count': count}
+    else:
+        return {'cart_count': 0}
